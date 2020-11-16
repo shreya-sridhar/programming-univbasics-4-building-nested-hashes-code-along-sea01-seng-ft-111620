808999b4c21e5c574236d78aa6ba4b79ee7cc51e
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-16 18:08:35 +0000

    Automatically backed up by Learn

--- spec/01_challenge_spec.rb
+++ spec/01_challenge_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/01_first_challenge.rb'
 
 describe "first_challenge" do
 	it "sets a variable, epic_tragedy, equal to a hash containing keys of family names and values of empty hashes" do
-    expect(first_challenge.keys).to eq([:montague, :capulet])
+    expect(first_challenge.keys).to eq([:montague,:capulet])
     expect(first_challenge.values).to eq([{}, {}])
   end
 end
